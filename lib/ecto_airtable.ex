35d26bdea1b5d44b1ccd86f42faeb6ae7e155841
Tymon Tobolski <i@teamon.eu>
2017-09-26 17:08:19 +0200

    init

---
+++ lib/ecto_airtable.ex
@@ -0,0 +1,18 @@
+defmodule EctoAirtable do
+  @moduledoc """
+  Documentation for EctoAirtable.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoAirtable.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
